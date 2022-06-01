ba24cc1591642311112b2244a4d8177100c025a8
Saad Amir <59224810+saadamirpk@users.noreply.github.com>
2022-06-02 00:35:49 +0500

    Parallax

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ function App() {
   return (
     <div className="App">
       <AppBar />
-
       <HeroSection />
     </div>
   );
--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,25 +1,77 @@
-import React from "react";
-import earth from "../Images/mid-earth.png";
-import rocket from "../Images/front-rocket.png";
-import stars from "../Images/back-stars.png";
-import space from "../Images/spacex.jpg";
+import React, { useEffect, useState } from "react";
+import back from "../Images/back.png";
+import fore_smoke from "../Images/fore-smoke.png";
+import fore_tower from "../Images/fore-tower.png";
+import mid_rocket from "../Images/mid-rocket.png";
+import mid_tower from "../Images/mid-tower.png";
 
 export default function HeroSection() {
-  //Where do aliens go for a drink? The space bar.
-  return (
-    <>
-      <div className="img-holder">
-        <h2 className="object" data-value="3">
-          My Text
-        </h2>
+  const [offsetY, setOffsetY] = useState(0);
+  const [offsetX, setOffsetX] = useState(0);
 
-        <img src={space} alt="Stars" id="star" />
-        {/** 
-      <img src={earth} alt="Earth" id="earth" />
-      <img src={rocket} alt="Rocket" id="rocket" />
-    */}
-      </div>
-      <div className="data"></div>
-    </>
+  const handleMouseMove = (e: any) => {
+    setOffsetY(e.screenY);
+    setOffsetX(e.screenX);
+  };
+
+  return (
+    <section onMouseMove={handleMouseMove} className="parallax">
+      <img
+        src={back}
+        style={{
+          transform: `translateX(${(offsetX * 1) / 100}px) translateY(${
+            (offsetY * 2) / 100
+          }px)`,
+        }}
+        alt="Background Image"
+        className="layer"
+      />
+      <img
+        src={mid_rocket}
+        style={{
+          transform: `translateX(${(offsetX * 2) / 100}px) translateY(${
+            (offsetY * 7) / 100
+          }px)`,
+        }}
+        alt="Background Image"
+        data-speed="3"
+        className="layer"
+      />
+      <img
+        src={mid_tower}
+        style={{
+          transform: `translateX(${(offsetX * 4) / 100}px) translateY(${
+            (offsetY * 3) / 100
+          }px)`,
+        }}
+        alt="Background Image"
+        data-speed="4"
+        className="layer"
+      />
+      <img
+        src={fore_tower}
+        style={{
+          transform: `translateX(${(offsetX * 3) / 100}px) translateY(${
+            (offsetY * 3) / 100
+          }px)`,
+        }}
+        alt="Background Image"
+        data-speed="-5"
+        className="layer"
+      />
+      <img
+        src={fore_smoke}
+        style={{
+          transform: `translateX(${(offsetX * -3) / 100}px) translateY(${
+            (offsetY * 3) / 100
+          }px)`,
+        }}
+        alt="Background Image"
+        data-speed="7"
+        className="layer"
+      />
+      {/* <h2 className="layer">Where do aliens go for a drink? The space bar.</h2>
+       */}
+    </section>
   );
 }
